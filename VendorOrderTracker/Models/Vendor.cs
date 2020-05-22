78d5d6b51199edb0119c0f035177eeb0b2e0bb32
unknown <macjay314@gmail.com>
2020-05-22 10:37:58 -0700

    Integrate orders into vendor class using a list of orders.

--- VendorOrderTracker/Models/Vendor.cs
+++ VendorOrderTracker/Models/Vendor.cs
@@ -8,13 +8,14 @@ namespace VendorOrderTracker.Models
     public string Description { get; set; }
     public int Id { get; }
     private static List<Vendor> _instances = new List<Vendor> {};
-
+    public List<Order> Orders { get; set; }
     public Vendor(string name, string description)
     {
       Name = name;
       Description = description;
       _instances.Add(this);
       Id = _instances.Count;
+      Orders = new List<Order>{};
     }
 
     public static List<Vendor> GetAll()
@@ -30,6 +31,11 @@ namespace VendorOrderTracker.Models
     public static void ClearAll()
     {
       _instances.Clear();
+    }
+
+    public void AddOrder(Order order)
+    {
+      Orders.Add(order);
     }
   }
 }
