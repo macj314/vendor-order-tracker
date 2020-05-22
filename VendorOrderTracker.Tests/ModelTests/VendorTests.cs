d7c2d91836244f080338070e60ac5bf6101c0844
unknown <macjay314@gmail.com>
2020-05-22 10:18:31 -0700

    Create and successfully run test to return vendor name.

--- VendorOrderTracker.Tests/ModelTests/VendorTests.cs
+++ VendorOrderTracker.Tests/ModelTests/VendorTests.cs
@@ -16,8 +16,17 @@ namespace VendorOrderTracker.Tests
     [TestMethod]
     public void VendorConstructor_CreateInstanceVendor_Object()
     {
-      Vendor vendor = new Vendor("testTitle", "testDesc");
+      Vendor vendor = new Vendor("testName", "testDesc");
       Assert.AreEqual(typeof(Vendor), vendor.GetType());
+    }
+
+    [TestMethod]
+    public void GetName_ReturnsName_String()
+    {
+      string name = "testName";
+      Vendor vendor = new Vendor(name, "testDesc");
+      string result = vendor.Name;
+      Assert.AreEqual(name, result);
     }
   }
 }
